dc7e5007ee4bed64fb3595f14be1161181974204
liangxiaojun <fatalxiao@163.com>
2023-03-06 10:01:36 +0800

    update dependencies

--- .babelrc.js
+++ .babelrc.js
@@ -3,49 +3,20 @@
  */
 
 const presets = [
-        [
-            '@babel/preset-env',
-            {
-                modules: false,
-                useBuiltIns: 'usage',
-                corejs: 3,
-                targets: {
-                    'safari': '7',
-                    'ie': '9'
-                }
+    [
+        '@babel/preset-env',
+        {
+            modules: false,
+            useBuiltIns: 'usage',
+            corejs: 3,
+            targets: {
+                'safari': '7',
+                'ie': '9'
             }
-        ],
-        '@babel/preset-react'
+        }
     ],
-    commonPlugins = [
-        ['@babel/plugin-proposal-decorators', {legacy: true}],
-        '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-proposal-do-expressions',
-        '@babel/plugin-proposal-export-default-from',
-        '@babel/plugin-proposal-export-namespace-from',
-        '@babel/plugin-proposal-function-bind',
-        '@babel/plugin-proposal-function-sent',
-        '@babel/plugin-proposal-json-strings',
-        '@babel/plugin-proposal-logical-assignment-operators',
-        '@babel/plugin-proposal-nullish-coalescing-operator',
-        '@babel/plugin-proposal-numeric-separator',
-        '@babel/plugin-proposal-optional-chaining',
-        ['@babel/plugin-proposal-pipeline-operator', {proposal: 'minimal'}],
-        '@babel/plugin-proposal-throw-expressions',
-        '@babel/plugin-syntax-dynamic-import',
-        '@babel/plugin-syntax-import-meta',
-        '@babel/plugin-transform-runtime'
-    ],
-    packagePlugins = [
-        ['transform-react-remove-prop-types', {removeImport: true}]
-    ],
-    packageConfig = {
-        presets,
-        plugins: [
-            ...commonPlugins,
-            ...packagePlugins
-        ]
-    };
+    '@babel/preset-react'
+];
 
 module.exports = {
     'env': {
@@ -53,7 +24,6 @@ module.exports = {
         'development': {
             presets,
             plugins: [
-                ...commonPlugins,
                 'transform-import-sync'
             ]
         },
@@ -65,7 +35,12 @@ module.exports = {
             ]
         },
 
-        'production': packageConfig
+        'production': {
+            presets,
+            plugins: [
+                ['transform-react-remove-prop-types', {removeImport: true}]
+            ]
+        }
 
     }
 };
